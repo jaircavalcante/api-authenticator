94f065aae4262c260128660905a40e3239049ac2
jcavalcante49@gmail.com <jair.mendes@keepcyan.com>
2023-02-19 17:06:05 -0300

    Create Scripts

--- src/main/resources/db/migration/V1_1_create-table-roles.sql
+++
@@ -1,6 +0,0 @@
-create table roles (
-    id bigint not null auto_increment,
-	role VARCHAR(50) not null,
-
-	primary key(id)
-);
--- src/main/resources/db/migration/V2__create_table_users.sql
+++ src/main/resources/db/migration/V2__create_table_users.sql
@@ -1,8 +1,7 @@
-create table users(
+CREATE TABLE IF NOT EXISTS users(
     id bigint not null auto_increment,
-	username VARCHAR(50) not null,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
 	password VARCHAR(200) not null,
 	email VARCHAR(100) not null,
 	enabled boolean not null,
